caa983dac51141ba668980db46391ceffac7568c
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-03-26 11:20:07 +0700

    UpdateCategory

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -14,8 +14,31 @@ namespace BulkyBookWeb.Controllers
         }
         public IActionResult Index()
         {
-            IEnumerable<Category> objCategoryList = _db.Categories.ToList(); 
+            IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
+        }
+        //Get action method
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        //POST action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
