90b2895490eb9b7cb88694a33a311c1aa40a21b6
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-06 10:50:30 +0700

    8 4 SwitchLabVideo

--- BasicFlow/Program.cs
+++ BasicFlow/Program.cs
@@ -16,22 +16,47 @@ namespace BasicFlow
             Console.WriteLine("Enter an age: ");
             int age = Int32.Parse(Console.ReadLine());
 
-            if (6 <= age && age < 11)
+            //if (6 <= age && age < 11)
+            //{
+            //    Console.WriteLine("Elementary");
+            //}
+            //else if (11 <= age && age < 15)
+            //{
+            //    Console.WriteLine("Middle School");
+            //}
+            //else if (15 <= age && age < 18)
+            //{
+            //    Console.WriteLine("High School");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Out of Range");
+            //}
+            switch (age)
             {
-                Console.WriteLine("Elementary");
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                    Console.WriteLine("Elementary");
+                    break;
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                    Console.WriteLine("Middle School");
+                    break;
+                case 15:
+                case 16:
+                case 17:
+                    Console.WriteLine("High School");
+                    break;
+                default:
+                    Console.WriteLine("Out of Range");
+                    break;
             }
-            else if (11 <= age && age < 15)
-            {
-                Console.WriteLine("Middle School");
-            }
-            else if (15 <= age && age < 18)
-            {
-                Console.WriteLine("High School");
-            }
-            else
-            {
-                Console.WriteLine("Out of Range");
-            }
+            Console.ReadLine();
         }
     }
 }
