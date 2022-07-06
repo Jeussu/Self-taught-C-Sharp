e931c347422d23d166752d6576d76449e761ab0f
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-06 21:41:19 +0700

    8 13 BreakContinueLabVideo

--- BasicFlow/Program.cs
+++ BasicFlow/Program.cs
@@ -6,18 +6,42 @@ namespace BasicFlow
     {
         static void Main(string[] args)
         {
-            int i = 1;
-            while (i++ < 6)
-            {
-                Console.WriteLine("Current value is {0}", i);
-            }
-            Console.WriteLine("\n Do While result");
-            i = 1;
+            //int i = 1;
+            //while (i++ < 6)
+            //{
+            //    Console.WriteLine("Current value is {0}", i);
+            //}
+            //Console.WriteLine("\n Do While result");
+            //i = 1;
 
-            do
+            //do
+            //{
+            //    Console.WriteLine("Current value is {0}", i);
+            //} while (i++ < 6);
+            /*
+             1. Allow user enter any character and only exit program if they enter Q or q
+             2. Allow user enter a number print out all even number <= inputted number.
+             */
+            //while (true)
+            //{
+            //    var input = Console.ReadLine();
+            //    if (input.Equals("Q") || input.Equals("q")) ;
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine("You have press: {0}", input);
+            //}
+            //Console.WriteLine("Done");
+            int intTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+            for (int i = 1; i < intTemp; i++)
             {
-                Console.WriteLine("Current value is {0}", i);
-            } while (i++ < 6);
+                if (i%2 != 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
             Console.ReadLine();
         }
     }
