b7dbd8d5ad5530562702597b706d66aa2e7491d3
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-07 15:31:21 +0700

    5 2 DateTimeLab

---
+++ StringAndDateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringAndDateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2030, 02, 26);
+            Console.WriteLine("dt's day of week: " + dt.DayOfWeek); // lấy về ngày đó trong tuần
+            Console.WriteLine("dt's day of year: " + dt.DayOfYear); // lấy về ngày thứ n trong năm
+            Console.WriteLine("dt: " + dt);
+
+            // get now time
+            DateTime now = DateTime.Now;
+            Console.WriteLine("now is: " + now);
+
+            DateTime utcTime = DateTime.UtcNow;
+            Console.WriteLine("Utc is: " + utcTime);
+
+
+            Console.ReadLine();
+        }
+    }
+}
