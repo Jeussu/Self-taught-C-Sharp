066b00ef44950ae885f03b2a6132ccd16665af88
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-09 10:52:46 +0700

    3 4 Class Lab Edited

---
+++ BasicOOP/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicOOP
+{
+    public class Dog
+    {
+        public int size;
+        public string breed;
+        public string name;
+
+        public Dog(int size, string breed, string name)
+        {
+            this.size = size;//this references cụ thể tới class dog
+            this.breed = breed;
+            this.name = name;
+        }
+
+
+        public void bark()
+        {
+            Console.WriteLine(name + " is a" + breed + ".He barks Ruff, Ruff");
+        }
+    }
+}
---
+++ BasicOOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasicOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog(14, "Bruno", "BullDog");
+
+            d.bark();//call method
+            Console.ReadKey();
+        }
+    }
+}
