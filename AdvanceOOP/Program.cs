2869b1d0dc39b99e4f788e99490315566e891bad
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-10 23:34:20 +0700

    13 4 ConstructorOverloading

--- AdvanceOOP/Program.cs
+++ AdvanceOOP/Program.cs
@@ -2,30 +2,55 @@
 
 namespace AdvanceOOP
 {
-    public class AddingWrapper
-    {//demo overloading
-        public int add (int a, int b)
-        {
-            return a + b;
+    //public class AddingWrapper
+    //{//demo overloading
+    //    public int add (int a, int b)
+    //    {
+    //        return a + b;
+    //    }
+
+    //    public int add(int a, int b, int c)
+    //    {
+    //        return a + b + c;
+    //    }
+
+    //    public float add(float a, float b, float c, float d)
+    //    {
+    //        return a + b + c + d;
+    //    }
+    //}
+
+    //Demo Constructor overloading 
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    
+        public Person (string firstName, string lastName)
+        {// Constructor 1
+            FirstName = firstName;
+            LastName = lastName;
         }
 
-        public int add(int a, int b, int c)
+        public Person(string firstName, string lastName, int age) : this (firstName, lastName) //calling
         {
-            return a + b + c;
-        }
-
-        public float add(float a, float b, float c, float d)
-        {
-            return a + b + c + d;
+            Age = age;
         }
     }
+
     public class Program
     {
         static void Main(string[] args)
         {
-            AddingWrapper aw = new AddingWrapper();
-            Console.WriteLine(aw.add (1, 2));
-            Console.WriteLine(aw.add(1, 2, 3, 4));
+            //AddingWrapper aw = new AddingWrapper();
+            //Console.WriteLine(aw.add (1, 2));
+            //Console.WriteLine(aw.add(1, 2, 3, 4));
+
+            Person person1 = new Person("Tien Dung", "Bui");
+            Person person2 = new Person("Quang Hai", "Nguyen", 21);
+            Console.WriteLine("Person1: " + person1);
+            Console.WriteLine("Person2: " + person2);
 
             Console.ReadLine();
         }
