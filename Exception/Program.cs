5994bd8965ec5fe8480c03e21ea0c4a1ebb63e92
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-12 11:00:43 +0700

    10 8 ThrowLabVideo

--- Exception/Program.cs
+++ Exception/Program.cs
@@ -10,27 +10,44 @@ namespace Exception
              Allow user enter 2 numbers and try catch divide zero.
             See properties of an exception.
              */
-            Console.WriteLine("Please enter 2 numbers:");
+            //Console.WriteLine("Please enter 2 numbers:");
+            //try
+            //{
+            //    int number1 = Int32.Parse(Console.ReadLine());
+            //    int number2 = Int32.Parse(Console.ReadLine());
+
+            //    int result = number1 / number2;
+            //    Console.WriteLine("Number1 is {0}, number2 is {1}, result is {2}", number1, number2, result);
+            //}
+            //catch (DivideByZeroException e)
+            //{
+            //    Console.WriteLine("Message: {0}", e.Message);
+            //    Console.WriteLine("StackTrace: {0}", e.StackTrace);//nơi diễn ra exception
+            //    Console.WriteLine("TargetSite: {0}", e.TargetSite);// chỉ ra method gây ra exception
+            //    Console.WriteLine("InnerException: {0}", e.InnerException);
+            //}
+            //catch(FormatException ex)
+            //{//convert trường hợp user nhập string sang int
+            //    Console.WriteLine("you have enter invalid param {0}", ex.Message);
+            //}
+
+            //Demo argument tạo ra exception
+            int[] test = new int[] {1, 2, 3};
+            GetValueFromArray(test, 5);
+
+            Console.ReadLine();
+        }
+        static int GetValueFromArray(int[] array, int index)
+        {
             try
             {
-                int number1 = Int32.Parse(Console.ReadLine());
-                int number2 = Int32.Parse(Console.ReadLine());
-
-                int result = number1 / number2;
-                Console.WriteLine("Number1 is {0}, number2 is {1}, result is {2}", number1, number2, result);
+                return array[index];
             }
-            catch (DivideByZeroException e)
+            catch (IndexOutOfRangeException ex)
             {
-                Console.WriteLine("Message: {0}", e.Message);
-                Console.WriteLine("StackTrace: {0}", e.StackTrace);//nơi diễn ra exception
-                Console.WriteLine("TargetSite: {0}", e.TargetSite);// chỉ ra method gây ra exception
-                Console.WriteLine("InnerException: {0}", e.InnerException);
+                var ex2 = new ArgumentException("Index is out of range", "index", ex);
+                throw ex2;
             }
-            catch(FormatException ex)
-            {//convert trường hợp user nhập string sang int
-                Console.WriteLine("you have enter invalid param {0}", ex.Message);
-            }
-            Console.ReadLine();
         }
     }
 }
