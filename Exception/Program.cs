3a900837c6e02fff8a122866130e4acc3940062d
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-12 10:11:38 +0700

    10 4 TryCatchLabVideo

---
+++ Exception/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Try Catch demo 
+             Allow user enter 2 numbers and try catch divide zero.
+            See properties of an exception.
+             */
+            Console.WriteLine("Please enter 2 numbers:");
+            try
+            {
+                int number1 = Int32.Parse(Console.ReadLine());
+                int number2 = Int32.Parse(Console.ReadLine());
+
+                int result = number1 / number2;
+                Console.WriteLine("Number1 is {0}, number2 is {1}, result is {2}", number1, number2, result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Message: {0}", e.Message);
+                Console.WriteLine("StackTrace: {0}", e.StackTrace);//nơi diễn ra exception
+                Console.WriteLine("TargetSite: {0}", e.TargetSite);// chỉ ra method gây ra exception
+                Console.WriteLine("InnerException: {0}", e.InnerException);
+            }
+            Console.ReadLine();
+        }
+    }
+}
