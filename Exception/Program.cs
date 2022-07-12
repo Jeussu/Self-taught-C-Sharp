461b23b5344bf288abdfe7f5ff71213da6cc5f5b
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-12 15:48:28 +0700

    10 11 CustomExceptionLab

--- Exception/Program.cs
+++ Exception/Program.cs
@@ -1,52 +1,40 @@
 ﻿using System;
 
-namespace Exception
+namespace Exception1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /* Try Catch demo 
-             Allow user enter 2 numbers and try catch divide zero.
-            See properties of an exception.
+            /* Allow user enter name and age. If age <0 throw an exception with name and age + "It cannot"
+             less than zero
              */
-            //Console.WriteLine("Please enter 2 numbers:");
-            //try
-            //{
-            //    int number1 = Int32.Parse(Console.ReadLine());
-            //    int number2 = Int32.Parse(Console.ReadLine());
-
-            //    int result = number1 / number2;
-            //    Console.WriteLine("Number1 is {0}, number2 is {1}, result is {2}", number1, number2, result);
-            //}
-            //catch (DivideByZeroException e)
-            //{
-            //    Console.WriteLine("Message: {0}", e.Message);
-            //    Console.WriteLine("StackTrace: {0}", e.StackTrace);//nơi diễn ra exception
-            //    Console.WriteLine("TargetSite: {0}", e.TargetSite);// chỉ ra method gây ra exception
-            //    Console.WriteLine("InnerException: {0}", e.InnerException);
-            //}
-            //catch(FormatException ex)
-            //{//convert trường hợp user nhập string sang int
-            //    Console.WriteLine("you have enter invalid param {0}", ex.Message);
-            //}
-
-            //Demo argument tạo ra exception
-            int[] test = new int[] {1, 2, 3};
-            GetValueFromArray(test, 5);
-
-            Console.ReadLine();
-        }
-        static int GetValueFromArray(int[] array, int index)
-        {
             try
             {
-                return array[index];
+                Console.WriteLine("Enter a name: ");
+                var name = Console.ReadLine();
+                Console.WriteLine("Enter your age");
+                int age = Int32.Parse(Console.ReadLine());
+                if (age < 0)
+                {
+                    throw new CustomException("{0}' s age is {1}. It cannot be less than zero", name, age);
+                }
             }
-            catch (IndexOutOfRangeException ex)
+            catch (CustomException ex)
             {
-                var ex2 = new ArgumentException("Index is out of range", "index", ex);
-                throw ex2;
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        public class CustomException : Exception
+        {
+            public CustomException() : base() { }
+            public CustomException(string message) : base(message) { }
+            public CustomException(string format, params object[] arguments) : base(string.Format(format, arguments))
+            {
+
             }
         }
     }
