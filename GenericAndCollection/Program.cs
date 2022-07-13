8d009f8d798ee3f59a0b03b7d5e8acb502e88cb3
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-13 10:53:32 +0700

    11 10 SortedList Lab

--- GenericAndCollection/Program.cs
+++ GenericAndCollection/Program.cs
@@ -7,31 +7,37 @@ namespace GenericAndCollection
     {
         static void Main(string[] args)
         {
-            //Add, AddRange, insert, remove, retrieve an item, using foreach
-            ArrayList t = new ArrayList();
-            t.Add(1);
-            t.Add("Hello");
-            t.Add(1.0m);
 
-            ArrayList t2 = new ArrayList();
-            t2.Add(10);
-            t2.AddRange(t);
+            /*//Demo SortedList
+            1. Declaration and Add
+            2. Access by key
+            3. Access by index
+            4. Check existing key
+            5. Clear
+             */
+            SortedList sl = new SortedList();
+            sl.Add(5, "Five");
+            sl.Add(4, "Four");
+            sl.Add(7, "Seven");
+            sl.Add(8, "Eight");
 
-            //foreach (var item in t2)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            t2.Insert(1, "new value");
-            t2.Add("new value");
-            t2.Remove("new value");
-            int firstItem = (int) t2[0];
-            foreach (var item in t2)
+            var keys = sl.Keys;
+            foreach (var key in keys)
             {
-                Console.WriteLine(item);
+                Console.WriteLine("Key is {0}, value is {1}", key, sl[key]);
             }
 
+            Console.WriteLine("-----------------");
 
+            //Getby index
+            for (int i = 0; i < sl.Count; i++)
+            {
+                Console.WriteLine("Key is {0}, value is {1}", sl.GetKey(i), sl.GetByIndex(i));
+            }
+
+            Console.WriteLine("6 is exists? " + sl.ContainsKey(6));
+            sl.Clear();
+            Console.WriteLine("After clearing" + sl.Count);
             Console.ReadLine();
         }
     }
