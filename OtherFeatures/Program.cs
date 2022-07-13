c8c12d8d53437b32836a84c527fd60bce8a41e8f
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-13 23:38:18 +0700

    DelegateLabVideo

--- OtherFeatures/Program.cs
+++ OtherFeatures/Program.cs
@@ -8,23 +8,31 @@ namespace OtherFeatures
 
     internal class Program
     {
+        public delegate void Print(int value);
+        public static void PrintNumber(int num)
+        {
+            Console.WriteLine("Number: {0, -12:NO}", num);
+        }
+
+        public static void PrintMoney(int money)
+        {
+            Console.WriteLine("Money: {0:C}", money);
+        }
+
+        public static void PrintHelper(Print delegateFunction, int numberToPrint)
+        {
+            delegateFunction(numberToPrint);
+        }
+
         static void Main(string[] args)
         {
 
-            //demo anonymous types
-            var player = new
-            {
-                firstName = "Tien Dung",
-                lastName = "Bui"
-            };
-
-            PrintName(player);
+            //demo Delegate
+            // passing a method as a parameter to another method.
+            PrintHelper(PrintNumber, 1000);
+            PrintHelper(PrintMoney, 200);
             Console.ReadKey();
         }
-        public static void PrintName(dynamic o)
-        {
 
-            Console.WriteLine("Player name is {0} {1}", o.firstName, o.lastName);
-        }
     }
 }
