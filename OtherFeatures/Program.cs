afc4217130376c0e8b11f80c81a480de2ac30378
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-14 15:59:39 +0700

    Tuple Demo

--- OtherFeatures/Program.cs
+++ OtherFeatures/Program.cs
@@ -5,40 +5,34 @@ using System.Linq;
 
 namespace OtherFeatures
 {
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
     //Demo Labda expression
 
     internal class Program
     {
-        public class Student
-        {
-            public string Name { get; set; }
-            public int Age { get; set; }
-        }
+
 
         static void Main(string[] args)
         {
-            //Write a lamda expression
-            // Using lamda with LINQ
-            Func<Student, bool> isTenenage = s => s.Age > 12 && s.Age < 20;
-
-            Student mike = new Student() { Name = "Mike", Age = 18 };
-            Console.WriteLine(isTenenage(mike));
-
-            IList<Student> students = new List<Student>()
-            {
-                new Student() { Name = "Mike", Age = 18 },
-                new Student() { Name = "Huong", Age = 20 },
-                new Student() { Name = "Lan", Age = 16 },
-                new Student() { Name = "Vy", Age = 21 },
-            };
-            //use linq to query get student is tenenage
-            var teenagers = students.Where(isTenenage).ToList();
-
-            foreach (var teen in teenagers)
-            {
-                Console.WriteLine("Name {0} and age {1}", teen.Name, teen.Age);
-            }
+            var result = DoSomething("", "", 0);
+            Console.WriteLine(result.Item1);
+            Console.WriteLine(result.Item2);
+            Console.WriteLine(result.Item3);
             Console.ReadKey();
+        }
+
+        public static Tuple<string, string, int>DoSomething(String firstName, String lastName, int age)
+        {
+            firstName = "A";
+            lastName = "B";
+            age = 100;
+            return Tuple.Create(firstName, lastName, age);
+            //return new Customer(firstName, lastName, age);
         }
 
     }
