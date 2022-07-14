baaea6b43de04b2a5b1be48c342e9c4b8f340182
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-14 16:22:02 +0700

    NullConditionalLabVideo

--- BasicFlow/Program.cs
+++ BasicFlow/Program.cs
@@ -1,47 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BasicFlow
 {
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public List<Address> Addresses { get; set; }
+        public Person Spouse { get; set; }
+    }
+
+    public class Address
+    {
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }    
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
     public class Program
     {
         static void Main(string[] args)
         {
-            //int i = 1;
-            //while (i++ < 6)
-            //{
-            //    Console.WriteLine("Current value is {0}", i);
-            //}
-            //Console.WriteLine("\n Do While result");
-            //i = 1;
-
-            //do
-            //{
-            //    Console.WriteLine("Current value is {0}", i);
-            //} while (i++ < 6);
-            /*
-             1. Allow user enter any character and only exit program if they enter Q or q
-             2. Allow user enter a number print out all even number <= inputted number.
-             */
-            //while (true)
-            //{
-            //    var input = Console.ReadLine();
-            //    if (input.Equals("Q") || input.Equals("q")) ;
-            //    {
-            //        break;
-            //    }
-            //    Console.WriteLine("You have press: {0}", input);
-            //}
-            //Console.WriteLine("Done");
-            int intTemp = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n");
-            for (int i = 1; i < intTemp; i++)
+            Person p = null;
+            if (p != null)
             {
-                if (i%2 != 0)
-                {
-                    continue;
-                }
-                Console.WriteLine(i);
+                Console.WriteLine(p.Age);
             }
+            Console.WriteLine(p?.Age);
+            if (p!= null && p.Spouse != null)
+            {
+                Console.WriteLine(p.Spouse.FirstName);
+            }
+
+            Console.WriteLine(p?.Spouse?.FirstName);
+            Console.WriteLine(p?.Addresses?[0].City);
+            Console.WriteLine("Done");
+
             Console.ReadLine();
         }
     }
