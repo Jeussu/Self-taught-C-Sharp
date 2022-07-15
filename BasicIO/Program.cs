011ac75824cfdf9d67941024e71d61ea0e46aa5b
Jeussu <94780400+Jeussu@users.noreply.github.com>
2022-07-15 15:40:10 +0700

    9 1 Path Video

---
+++ BasicIO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace BasicIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\IO\demo.txt";
+            Console.WriteLine(Path.GetDirectoryName(path1));
+            Console.WriteLine(Path.GetFileName(path1));
+            Console.WriteLine(Path.GetFileNameWithoutExtension(path1));
+            Console.WriteLine(Path.GetFullPath("."));
+        }
+    }
+}
